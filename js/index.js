3b42c29566c7cf734b6a3fa3f80dce14b22a042a
Edwin Pérez <72474341+EdwinPerez23@users.noreply.github.com>
2021-01-08 16:38:18 -0500

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const images = document.querySelectorAll(".img");
+const containerImage = document.querySelector(".container-img");
+const immageContainer = document.querySelector(".img-show");
+const copy = document.querySelector(".copy");
+const closeModal = document.querySelector(".bx.bx-x")
+
+images.forEach(image => {
+    image.addEventListener("click", () => {
+        
+        addImage(image.getAttribute("src"),image.getAttribute("alt"));
+    })
+})
+
+const addImage = (srcImage, altImage) => {
+    containerImage.classList.toggle("move");
+    immageContainer.classList.toggle("show");
+    immageContainer.src = srcImage;
+    copy.innerHTML = altImage;
+}
+
+closeModal.addEventListener("click", () => {
+    containerImage.classList.toggle("move");
+    immageContainer.classList.toggle("show");
+})
